1fab4bea6990a293617e7566f8622c46ca4db4db
Mark Petersen <markiepetersen@yahoo.com>
2016-03-30 13:42:21 -0600

    added algorithm 15 to minimize J

--- catkin_ws/src/Trim.py
+++ catkin_ws/src/Trim.py
@@ -3,14 +3,11 @@ from numpy import linalg
 import Param as P
 
 class Trim():
-    def __init__(self,Va,gamma,R,alpha,beta,phi):
+    def __init__(self,Va,gamma,R):
         # These parameters are specified by the user
         self.Va = Va        # Trim airspeed, m/s
         self.gamma = gamma  # Trim flight path angle, rad
         self.R = R          # Trim turn radius, m
-        self.alpha = alpha  # Trim angle of attack, rad
-        self.beta = beta    # Trim side slip angle, rad
-        self.phi = phi      # Trim roll angle, rad
 
         # These parameters are calculated based off of the specified parameters above
 
@@ -46,40 +43,57 @@ class Trim():
         self.Z_delta_e = 0.0
 
         # fx
-        self.pndot = 0.0
-        self.pedot = 0.0
-        self.hdot = 0.0
-        self.udot = 0.0
-        self.vdot = 0.0
-        self.wdot = 0.0
-        self.phidot = 0.0
-        self.thetadot = 0.0
-        self.psidot = 0.0
-        self.pdot = 0.0
-        self.qdot = 0.0
-        self.rdot = 0.0
+        self.fx = np.zeros(12,1)
 
-    def getBodyFrameVelocities(self):
-        self.u = self.Va*np.cos(self.alpha)*np.cos(self.beta)
-        self.v = self.Va*np.sin(self.beta)
-        self.w = self.Va*np.sin(self.alpha)*np.cos(self.beta)
 
-    def getPitchAngle(self):
-        self.theta = self.alpha + self.gamma
+        #xdotTrim
+        self.xdotTrim = np.matrix([[0],                                 # pn dot
+                               [0],                                 # pe dot
+                               [self.Va*np.sin(self.gamma)],        # h dot
+                               [0],                                 # u dot
+                               [0],                                 # v dot
+                               [0],                                 # w dot
+                               [0],                                 # phi dot
+                               [0],                                 # theta dot
+                               [self.Va/self.R*np.cos(self.gamma)], # psi dot
+                               [0],                                 # p dot
+                               [0],                                 # q dot
+                               [0]])                                # r dot 
 
-    def getAngularRates(self):
+        self.alpha_dl = self.gamma
+        self.beta_dl = 0.0
+        self.phi_dl = 0.0
+        self.J_dl = 0.0
+
+        self.alpha = 0.0
+        self.beta = 0.0
+        self.phi = 0.0
+        self.J = 0.0
+
+        self.epsilon = 0.05
+        self.K = 0.5
+
+    def getBodyFrameVelocities(self, alpha, beta):
+        self.u = self.Va*np.cos(alpha)*np.cos(beta)
+        self.v = self.Va*np.sin(beta)
+        self.w = self.Va*np.sin(alpha)*np.cos(beta)
+
+    def getPitchAngle(self,alpha):
+        self.theta = alpha + self.gamma
+
+    def getAngularRates(self,phi):
 
         self.p = -self.Va/self.R*np.sin(self.theta)
-        self.q = self.Va/self.R*np.sin(self.phi)*np.cos(self.theta)
-        self.r = self.Va/self.R*np.cos(self.phi)*np.cos(self.theta)
+        self.q = self.Va/self.R*np.sin(phi)*np.cos(self.theta)
+        self.r = self.Va/self.R*np.cos(phi)*np.cos(self.theta)
         # print('p %5.4e' % self.p)
 
 
 
-    def getElevator(self):
+    def getElevator(self,alpha):
         # The code is broken down into several steps to increase readability
         temp1 = (P.Jxz*(self.p**2 - self.r**2) + (P.Jx - P.Jz)*self.p*self.r)/(1/2*P.rho*self.Va**2*P.c*P.S)
-        temp2 = temp1 - P.C_m_0 - P.C_m_alpha*self.alpha - P.C_m_q*(P.c*self.q/(2*self.Va))
+        temp2 = temp1 - P.C_m_0 - P.C_m_alpha*alpha - P.C_m_q*(P.c*self.q/(2*self.Va))
 
         self.delta_e = temp2/P.C_m_delta_e
 
@@ -91,7 +105,7 @@ class Trim():
         temp4 = self.Va**2/P.kmotor**2
         self.delta_t = np.sqrt(temp3 + temp4)
 
-    def getAileronAndRudder(self):
+    def getAileronAndRudder(self,beta):
         # The code is broken down into several steps to increase readability
         matrix1 = np.matrix([[P.C_p_delta_a, P.C_p_delta_r], [P.C_r_delta_a, P.C_r_delta_r]])
         matrix1 = linalg.inv(matrix1)
@@ -104,41 +118,41 @@ class Trim():
         temp5 = P.C_r_p*(P.b*self.p/(2*self.Va))
         temp6 = P.C_r_r*(P.b*self.r/(2*self.Va))
 
-        matrix2 = np.matrix([[temp1 - P.C_p_0 - P.C_p_beta*self.beta - temp2 - temp3],
-            [temp4 - P.C_r_0 - P.C_r_beta*self.beta - temp5 - temp6]])
+        matrix2 = np.matrix([[temp1 - P.C_p_0 - P.C_p_beta*beta - temp2 - temp3],
+            [temp4 - P.C_r_0 - P.C_r_beta*beta - temp5 - temp6]])
 
         controls = matrix1*matrix2
 
         self.delta_a = controls[0,0]
         self.delta_r = controls[1,0]
 
-    def getSigma(self):
+    def getSigma(self,alpha):
         # The code is broken down into several steps to increase readability
-        numerator = 1+np.exp(-P.M*(self.alpha-P.alpha_0))+np.exp(P.M*(self.alpha+P.alpha_0))
-        den = ((1 + np.exp(-P.M*(self.alpha-P.alpha_0)))*(1+np.exp(P.M*(self.alpha+P.alpha_0))))
+        numerator = 1+np.exp(-P.M*(alpha-P.alpha_0))+np.exp(P.M*(alpha+P.alpha_0))
+        den = ((1 + np.exp(-P.M*(alpha-P.alpha_0)))*(1+np.exp(P.M*(alpha+P.alpha_0))))
         self.sigma = numerator/den
 
-    def getLiftModelCoeff(self):
-        self.C_L =  (1-self.sigma)*(P.C_L_0 + P.C_L_alpha*self.alpha) + \
-        self.sigma*(2*np.sign(self.alpha)*np.sin(self.alpha)**2*np.cos(self.alpha))
+    def getLiftModelCoeff(self,alpha):
+        self.C_L =  (1-self.sigma)*(P.C_L_0 + P.C_L_alpha*alpha) + \
+        self.sigma*(2*np.sign(alpha)*np.sin(alpha)**2*np.cos(alpha))
 
-    def getDragModelCoeff(self):
+    def getDragModelCoeff(self,alpha):
         AR = P.b**2/P.S
-        self.C_D =P.C_D_p + (P.C_L_0 + P.C_L_alpha*self.alpha)**2/(np.pi*P.e*AR)
+        self.C_D =P.C_D_p + (P.C_L_0 + P.C_L_alpha*alpha)**2/(np.pi*P.e*AR)
 
-    def getLiftCoeff(self):
-        self.getSigma()
-        self.getLiftModelCoeff()
-        self.getDragModelCoeff()
+    def getLiftCoeff(self,alpha):
+        self.getSigma(alpha)
+        self.getLiftModelCoeff(alpha)
+        self.getDragModelCoeff(alpha)
 
 
 
-        self.C_X =           -self.C_D*np.cos(self.alpha)  +          self.C_L*np.sin(self.alpha)
-        self.C_X_q =         -P.C_D_q*np.cos(self.alpha) +            P.C_L_q*np.sin(self.alpha)
-        self.C_X_delta_e =   -P.C_D_delta_e*np.cos(self.alpha) +      P.C_L_delta_e*np.sin(self.alpha)
-        self.C_Z =           -self.C_D*np.sin(self.alpha) -           self.C_L*np.cos(self.alpha)
-        self.C_Z_q =         -P.C_D_q*np.sin(self.alpha)-             P.C_L_q*np.cos(self.alpha)
-        self.C_Z_delta_e =   -P.C_D_delta_e*np.sin(self.alpha) -      P.C_L_delta_e*np.cos(self.alpha)
+        self.C_X =           -self.C_D*np.cos(alpha)  +          self.C_L*np.sin(alpha)
+        self.C_X_q =         -P.C_D_q*np.cos(alpha) +            P.C_L_q*np.sin(alpha)
+        self.C_X_delta_e =   -P.C_D_delta_e*np.cos(alpha) +      P.C_L_delta_e*np.sin(alpha)
+        self.C_Z =           -self.C_D*np.sin(alpha) -           self.C_L*np.cos(alpha)
+        self.C_Z_q =         -P.C_D_q*np.sin(alpha)-             P.C_L_q*np.cos(alpha)
+        self.C_Z_delta_e =   -P.C_D_delta_e*np.sin(alpha) -      P.C_L_delta_e*np.cos(alpha)
 
         # print('Sigma %5.4e' % self.sigma)
         # print('CL', self.C_L)
@@ -150,14 +164,14 @@ class Trim():
         # print('C_Z_q ', self.C_Z_q)
         # print('C_Z_delta_e', self.C_Z_delta_e)
 
-    def compute_X_Trim_U_Trim(self):
-        self.getLiftCoeff()
-        self.getBodyFrameVelocities()
-        self.getPitchAngle()
-        self.getAngularRates()
-        self.getElevator()
+    def compute_X_Trim_U_Trim(self,alpha,beta,phi):
+        self.getLiftCoeff(alpha)
+        self.getBodyFrameVelocities(alpha,beta)
+        self.getPitchAngle(alpha)
+        self.getAngularRates(phi)
+        self.getElevator(alpha)
         self.getThrottle()
-        self.getAileronAndRudder()
+        self.getAileronAndRudder(beta)
 
     def printValues(self):
         print('\n\n x trim')
@@ -181,69 +195,110 @@ class Trim():
         print('delta r', self.delta_r)
 
         print('\n\n xhatdot')
-        print('pndot', self.pndot)
-        print('pedot', self.pedot)
-        print('hdot', self.hdot)
-        print('udot', self.udot)
-        print('vdot', self.vdot)
-        print('wdot', self.wdot)
-        print('phidot', self.phidot)
-        print('thetadot', self.thetadot)
-        print('psidot', self.psidot)
-        print('pdot', self.pdot)
-        print('qdot', self.qdot)
-        print('rdot', self.rdot)
+        print('pndot', self.fx[0,0])
+        print('pedot', self.fx[1,0])
+        print('hdot', self.fx[2,0])
+        print('udot', self.fx[3,0])
+        print('vdot', self.fx[4,0])
+        print('wdot', self.fx[5,0])
+        print('phidot', self.fx[6,0])
+        print('thetadot', self.fx[7,0])
+        print('psidot', self.fx[8,0])
+        print('pdot', self.fx[9,0])
+        print('qdot', self.fx[10,0])
+        print('rdot', self.fx[11,0])
 
-    def get_f_x_u(self):
+    def get_f_x_u(self,alpha,beta,phi):
         # Calculates xhatdot
 
-        cos_phi = np.cos(self.phi)
+        self.compute_X_Trim_U_Trim(alpha,beta,phi)
+
+        cos_phi = np.cos(phi)
         cos_theta = np.cos(self.theta)
         cos_psi = np.cos(self.psi)
-        sin_phi = np.sin(self.phi)
+        sin_phi = np.sin(phi)
         sin_theta = np.sin(self.theta)
         sin_psi = np.sin(self.psi)
 
         temp1 = P.rho*Va**2*P.S/(2*P.m)
 
-        self.pndot = (cos_theta*cos_psi*self.u + (sin_phi*sin_theta*cos_psi - 
+        pndot = (cos_theta*cos_psi*self.u + (sin_phi*sin_theta*cos_psi - 
             cos_phi*sin_psi)*self.v + (cos_phi*sin_theta*cos_psi + 
             sin_phi*sin_psi)*self.w)
 
-        self.pedot = (cos_theta*cos_psi*self.u + (sin_phi*sin_theta*sin_psi +
+        pedot = (cos_theta*cos_psi*self.u + (sin_phi*sin_theta*sin_psi +
             cos_phi*cos_psi)*self.v + (cos_phi*sin_theta*sin_psi - 
             sin_phi*cos_psi)*self.w)
 
-        self.hdot = (self.u*sin_theta - self.v*sin_phi*cos_theta -
+        hdot = (self.u*sin_theta - self.v*sin_phi*cos_theta -
             self.w*cos_phi*cos_theta)
 
-        self.udot = (self.r*self.v - self.q*self.w - P.g*sin_theta + 
+        udot = (self.r*self.v - self.q*self.w - P.g*sin_theta + 
             temp1*(self.C_X + self.C_X_q*P.c*self.q/(2*self.Va) + self.C_X_delta_e*self.delta_e) +
             P.rho*P.Sprop*P.Cprop/(2*P.m)*((P.kmotor*self.delta_t)**2 - self.Va**2))
 
-        self.vdot = (self.p*self.w - self.r*self.u + P.g*cos_theta*sin_phi + temp1*
-            (P.C_Y_0 + P.C_Y_beta*self.beta + P.C_Y_p*P.b*self.p/(2*self.Va) + P.C_Y_r*P.b*self.r/(2*self.Va) +
+        vdot = (self.p*self.w - self.r*self.u + P.g*cos_theta*sin_phi + temp1*
+            (P.C_Y_0 + P.C_Y_beta*beta + P.C_Y_p*P.b*self.p/(2*self.Va) + P.C_Y_r*P.b*self.r/(2*self.Va) +
              P.C_Y_delta_a*self.delta_a + P.C_Y_delta_r*self.delta_r))
 
-        self.wdot = (self.q*self.u - self.p*self.v + P.g*cos_theta*cos_phi + temp1*(self.C_Z + 
+        wdot = (self.q*self.u - self.p*self.v + P.g*cos_theta*cos_phi + temp1*(self.C_Z + 
             self.C_Z_q*P.c*self.q/(2*self.Va) + self.C_Z_delta_e*self.delta_e))
 
-        self.phidot = self.p + self.q*sin_phi*np.tan(self.theta) + self.r*cos_phi*np.tan(self.theta)
+        phidot = self.p + self.q*sin_phi*np.tan(self.theta) + self.r*cos_phi*np.tan(self.theta)
 
-        self.thetadot = self.q*cos_phi - self.r*sin_phi
+        thetadot = self.q*cos_phi - self.r*sin_phi
 
-        self.psidot = self.q*sin_phi*(1/cos_theta) + self.r*cos_phi*(1/cos_theta)
+        psidot = self.q*sin_phi*(1/cos_theta) + self.r*cos_phi*(1/cos_theta)
 
-        self.pdot = (P.Gamma1*self.p*self.q - P.Gamma2*self.q*self.r + (1/2)*P.rho*self.Va**2*P.S*P.b*
-            (P.C_p_0 + P.C_p_beta*self.beta + P.C_p_p*P.b*self.p/(2*self.Va) + P.C_p_r*P.b*self.r/(2*self.Va) +
+        pdot = (P.Gamma1*self.p*self.q - P.Gamma2*self.q*self.r + (1/2)*P.rho*self.Va**2*P.S*P.b*
+            (P.C_p_0 + P.C_p_beta*beta + P.C_p_p*P.b*self.p/(2*self.Va) + P.C_p_r*P.b*self.r/(2*self.Va) +
              P.C_p_delta_a*self.delta_a + P.C_p_delta_r*self.delta_r))
 
-        self.qdot = (P.Gamma5*self.p*self.r - P.Gamma6*(self.p**2 - self.r**2) + P.rho*self.Va**2*P.S*P.c/(2*P.Jy)*
+        qdot = (P.Gamma5*self.p*self.r - P.Gamma6*(self.p**2 - self.r**2) + P.rho*self.Va**2*P.S*P.c/(2*P.Jy)*
             (P.C_m_0 + P.C_m_alpha*self.alpha + P.C_m_q*P.c*self.q/(2*self.Va) + P.C_m_delta_e*self.delta_e))
 
-        self.rdot = (P.Gamma7*self.p*self.q - P.Gamma1*self.q*self.r + (1/2)*P.rho*self.Va**2*P.S*P.b*
-            (P.C_r_0 + P.C_r_beta*self.beta + P.C_r_p*P.b*self.p/(2*self.Va) + P.C_r_r*P.b*self.r/(2*self.Va) +
+        rdot = (P.Gamma7*self.p*self.q - P.Gamma1*self.q*self.r + (1/2)*P.rho*self.Va**2*P.S*P.b*
+            (P.C_r_0 + P.C_r_beta*beta + P.C_r_p*P.b*self.p/(2*self.Va) + P.C_r_r*P.b*self.r/(2*self.Va) +
             P.C_r_delta_a*self.delta_a + P.C_r_delta_r*self.delta_r))
+
+        self.fx = np.matrix([[pndot],
+                             [pedot],
+                             [hdot],
+                             [udot],
+                             [vdot],
+                             [wdot],
+                             [phidot],
+                             [thetadot],
+                             [psidot],
+                             [pdot],
+                             [qdot],
+                             [rdot]])
+
+    
+    def getJ(self,alpha,beta,phi):
+
+        self.get_f_x_u()
+        return np.norm(self.xdotTrim - self.fx)**2
+
+
+
+    def minimizeJ(self):
+
+        N = 100;
+
+        for x in range(0, N):
+            self.alpha = self.alpha_dl + self.epsilon
+            self.beta = self.beta_dl + self.epsilon
+            self.phi = self.phi_dl + self.epsilon
+            self.J_dl = self.getJ(self.alpha_dl,self.beta_dl,self.phi_dl)
+            dJ_dAlpha = (self.getJ(self.alpha,self.beta_dl,self.phi_dl) - self.J_dl)/self.epsilon
+            dJ_dBeta = (self.getJ(self.alpha_dl,self.beta,self.phi_dl) - self.J_dl)/self.epsilon
+            dJ_dPhi = (self.getJ(self.alpha_dl,self.beta_dl,self.phi) - self.J_dl)/self.epsilon
+
+            self.alpha_dl = self.alpha_dl -self.K*dJ_dAlpha
+            self.beta_dl = self.beta_dl - self.K*dJ_dBeta
+            self.phi_dl = self.phi_dl - self.K*dJ_dPhi
+
 
 
 
